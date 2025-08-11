a72e99bffe978821ca3de08495c155d2ad147a8e
Tobias Morton <truemetro111@gmail.com>
2025-08-10 22:51:28 -0400

    fixed package name

--- scripts/system.sh
+++ scripts/system.sh
@@ -2,4 +2,4 @@
 # this script gets you up and running with a fairly minimal hyprland install
 
 # 2: install core utilities
-sudo pacman -S --needed amd-ucode networkmanager neovim base-devel git man-db man-pages jdk-openjdk-21 iw mesa vulkan-radeon pavucontrol unzip
+sudo pacman -S --needed amd-ucode networkmanager neovim base-devel git man-db man-pages jdk21-openjdk iw mesa vulkan-radeon pavucontrol unzip
